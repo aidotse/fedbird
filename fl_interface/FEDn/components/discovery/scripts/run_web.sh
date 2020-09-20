0e02de16d93351129b04b9aedebab934df360d65
Alexandre Charpy <alexandre.charpy@qamcom.se>
2020-09-21 00:08:02 +0200

    Add discovery and dashboard components - may split mongo and discovery

---
+++ fl_interface/FEDn/components/discovery/scripts/run_web.sh
@@ -0,0 +1,4 @@
+python3 manage.py makemigrations combiner
+python3 manage.py migrate
+python3 manage.py loaddata seed.json
+python3 manage.py runserver 0.0.0.0:8080
--- fl_interface/FEDn/start_discovery.sh
+++ fl_interface/FEDn/start_discovery.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 ARGUMENT_LIST=(
-    "client-name"
-    "platform"
     "component-path"
-    "config"
 )
 
 opts=$(getopt \
@@ -18,26 +15,10 @@ eval set --$opts
 
 while [[ $# -gt 0 ]]; do
     case "$1" in
-        --client-name)
-            argClientName=$2
-            shift 2
-            ;;
-
-        --platform)
-            argPlatform=$2
-            shift 2
-            ;;
-
 	--component-path)
             argComponentPath=$2
             shift 2
             ;;
-
-	--config)
-            argConfig=$2
-            shift 2
-            ;;
-
         *)
             break
             ;;
@@ -45,22 +26,13 @@ while [[ $# -gt 0 ]]; do
 done
 
 echo "======================================="
-echo "CLIENT_NAME = $argClientName"
-echo "PLATFORM = $argPlatform"
 echo "COMPONENT_PATH = $argComponentPath"
-echo "CONFIG = $argConfig"
 echo "======================================="
 
 COMPOSE_API_VERSION=1.40 \
-CLIENT_NAME=$argClientName \
-PLATFORM=$argPlatform \
 COMPONENT_PATH=$argComponentPath \
-CONFIG=$argConfig \
-docker-compose --env-file project.env -f client.yaml build
+docker-compose --env-file project.env -f mongo_and_discovery.yaml build
 
 COMPOSE_API_VERSION=1.40 \
-CLIENT_NAME=$argClientName \
-PLATFORM=$argPlatform \
 COMPONENT_PATH=$argComponentPath \
-CONFIG=$argConfig \
-docker-compose --env-file project.env -f client.yaml up #--remove-orphans
+docker-compose --env-file project.env -f mongo_and_discovery.yaml up #--remove-orphans
