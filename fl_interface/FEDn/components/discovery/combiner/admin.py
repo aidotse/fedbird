bec8db76e9760db66503ecfb0ad5ef5fcacc22c5
Alexandre Charpy <alexandre.charpy@qamcom.se>
2020-09-29 20:30:25 +0200

    Remove discovery components

--- fl_interface/FEDn/components/discovery/combiner/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import Combiner, CombinerConfiguration
-
-admin.site.register(Combiner)
-admin.site.register(CombinerConfiguration)
