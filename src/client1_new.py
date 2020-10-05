ba73af8f729ac985cf4a4960b06670b19d77f6a2
Sheetalr24 <sheetal.reddy@ai.se>
2020-10-05 16:24:54 +0200

    Add TrainingProcess class

--- src/client1_new.py
+++ src/client1_new.py
@@ -19,7 +19,8 @@ from sklearn.utils import shuffle
 from sklearn.metrics import accuracy_score
 
 import tensorflow as tf
-from keras.models import Model
+import keras
+#from keras.models import Model 
 from keras.callbacks import TensorBoard, ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
 
 from keras.layers import Input, Lambda
@@ -34,24 +35,17 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 
 class Model:
 
-   def __init__(self,input_shape=(416,416 )):
+   def __init__(self,input_shape=(416,416)):
 
-      self.anchors_path = 'model_data/tiny_yolo_anchors.txt'
-      self.classes_path = 'model_data/seabird_classes.txt'
-      self.class_names = get_classes(classes_path)
-      num_classes = len(class_names)
-      anchors = get_anchors(anchors_path)
-      self.lr = 0.001 
-      self.iteration_num = 100
       self.input_shape = input_shape
-
+      self.current_model = None
 
    @staticmethod
-   def build_model(input_shape, anchors, classes, load_pretrained=False, freeze_body=2, weights_path='model_data/yolov3-tiny.h5'):
+   def build_model(anchors, classes, load_pretrained=False, freeze_body=2, weights_path='model_data/yolov3-tiny.h5'):
         '''create the training model, for Tiny YOLOv3'''
         K.clear_session() # get a new session
         image_input = Input(shape=(None, None, 3))
-        h, w = input_shape
+        h, w = self.input_shape
         num_anchors = len(anchors)
 
         y_true = [Input(shape=(h//{0:32, 1:16}[l], w//{0:32, 1:16}[l], \
@@ -72,11 +66,69 @@ class Model:
         model_loss = Lambda(yolo_loss, output_shape=(1,), name='yolo_loss',
             arguments={'anchors': anchors, 'num_classes': num_classes, 'ignore_thresh': 0.7})(
             [*model_body.output, *y_true])
-        model = Model([model_body.input, *y_true], model_loss)
+        self.current_model = keras.models.Model([model_body.input, *y_true], model_loss)
 
-        return model
 
-   def train(init_epoch, client_name,iteration_num, clients_batched, epoch, anchors, num_classes, smlp_global):
+class TrainDataReader:
+    
+    # parameters initializiation
+    def __init__(self, annotation_path= '/data/Annotation/list1.txt',val_split=0.1):
+      self.annotation_path = annotation_path
+      self.val_split = val_split
+      self.batch_size = 32
+      
+    
+    # get the data from files
+    def read_training_data():
+        with open(self.annotation_path) as f:
+            lines = f.readlines()
+        np.random.seed(10101)
+        np.random.shuffle(lines)
+        np.random.seed(None)
+        num_val = int(len(lines)*self.val_split)
+        num_train = len(lines) - num_val
+        return lines[:,num_train], lines[num_train,:]
+
+    def data_generator(annotation_lines, input_shape, anchors, num_classes):
+        '''data generator for fit_generator'''
+        n = len(annotation_lines)
+        i = 0
+        while True:
+            image_data = []
+            box_data = []
+            for b in range(self.batch_size):
+                if i==0:
+                    np.random.shuffle(annotation_lines)
+                image, box = get_random_data(annotation_lines[i], input_shape, random=True)
+                image_data.append(image)
+                box_data.append(box)
+                i = (i+1) % n
+            image_data = np.array(image_data)
+            box_data = np.array(box_data)
+            y_true = preprocess_true_boxes(box_data, input_shape, anchors, num_classes)
+            yield [image_data, *y_true], np.zeros(self.batch_size)
+
+    def data_generator_wrapper(annotation_lines, input_shape, anchors, num_classes):
+        n = len(annotation_lines)
+        if n==0 or batch_size<=0: return None
+        return data_generator(annotation_lines, self.batch_size, input_shape, anchors, num_classes)
+
+
+class TrainingProcess:
+
+    def __init__(data, model, input_shape = (416,416)):
+        self.init_epoch = 0
+        self.end_epoch = 1 
+        self._data = data
+        self._model = model
+        self.anchors_path = 'model_data/tiny_yolo_anchors.txt'
+        self.classes_path = 'model_data/seabird_classes.txt'
+        self.class_names = get_classes(classes_path)
+        self.num_classes = len(class_names)
+        self.anchors = get_anchors(anchors_path)
+        self.input_shape = input_shape
+      
+    def train(init_epoch, num_classes, batch_size, epoch, lr):
         """Client main function to read the global model files, update the weights, and save the local model
 
         Parameters
@@ -92,131 +144,85 @@ class Model:
         epoch : number of local iteration before sending model to server
 
         """
-        num_train = clients_batched[0]
-        num_val = clients_batched[1]
-        batch_size = clients_batched[2]
+        '''num_train = clients_batched[0]
+        num_val = clients_batched[1]'''
+        
+        lines_train, lines_val = self._data.read_training_data()
         log_dir = 'logs/000/'
         logging = TensorBoard(log_dir=log_dir)
         checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-            monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
+        monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
         reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
         early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
-        print('Client name: %s connecting to server......' % (client_name))
-
-        local_model = smlp_global.build(input_shape,anchors,num_classes)
+        self._model.build_model() # model created, self._model.current_model
+        local_model = self._model.current_model  #check it once again, shallow copy? what datatype is local_model
+        end_epoch = self.init_epoch + self.epoch 
         while True:
+        '''if os.path.isfile("".join([Client_dir,'client_data.csv'])):
+         os.remove("".join([Client_dir,'client_data.csv']))
+        # get server data name and epoch
 
-            '''if os.path.isfile("".join([Client_dir,'client_data.csv'])):
-                os.remove("".join([Client_dir,'client_data.csv']))
-            # get server data name and epoch
+        if (os.path.isfile("".join([Server_dir,'server_data.csv']))):
+         server_data = pd.read_csv("".join([Server_dir,'server_data.csv']))
 
-            if (os.path.isfile("".join([Server_dir,'server_data.csv']))):
-                server_data = pd.read_csv("".join([Server_dir,'server_data.csv']))
+         if (int(server_data['epoch'])) == iteration_num + epoch:
+             break;
+         if (int(server_data['epoch'])) == init_epoch:
+             iteration_num = int(server_data['iteration_num'])
+             
+             #K.clear_session()
+             local_model.load_weights("".join([Server_dir,'global_model.h5']))
+             end_epoch = init_epoch + epoch'''
 
-                if (int(server_data['epoch'])) == iteration_num + epoch:
-                    break;
-                if (int(server_data['epoch'])) == init_epoch:
-                    iteration_num = int(server_data['iteration_num'])
-                    
-                    #K.clear_session()
-                    local_model.load_weights("".join([Server_dir,'global_model.h5']))
-                    end_epoch = init_epoch + epoch'''
+             # fit local model with client's data
+             #local_model.fit(clients_batched, epochs=epoch, verbose=1)
+             if (init_epoch<50):
+                 local_model.compile(optimizer=Adam(lr=1e-3), loss={ # use custom yolo_loss Lambda layer.
+                                     'yolo_loss': lambda y_true, y_pred: y_pred})
 
-                    # fit local model with client's data
-                    #local_model.fit(clients_batched, epochs=epoch, verbose=1)
-                    if (init_epoch<50):
-                        local_model.compile(optimizer=Adam(lr=1e-3), loss={ # use custom yolo_loss Lambda layer.
-                                            'yolo_loss': lambda y_true, y_pred: y_pred})
+                 
+                 local_model.fit_generator(self._data.data_generator_wrapper(lines_train ,self.input_shape, self.anchors, self.num_classes),
+                 steps_per_epoch=max(1, len(lines_train)//self._data.batch_size),
+                 validation_data=data_generator_wrapper(lines[num_train:], self.input_shape, self.anchors, self.num_classes),
+                 validation_steps=max(1, len(lines_val)//self._data.batch_size),
+                 epochs=end_epoch,
+                 initial_epoch=self.init_epoch,
+                 callbacks=[logging, checkpoint])
+             else:
+                 for i in range(len(local_model.layers)):
+                     local_model.layers[i].trainable = True
+                 local_model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
+                 print('Unfreeze all of the layers.')
 
-                        
-                        local_model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
-                        steps_per_epoch=max(1, num_train//batch_size),
-                        validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
-                        validation_steps=max(1, num_val//batch_size),
-                        epochs=end_epoch,
-                        initial_epoch=init_epoch,
-                        callbacks=[logging, checkpoint])
-                    else:
-                        for i in range(len(local_model.layers)):
-                            local_model.layers[i].trainable = True
-                        local_model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
-                        print('Unfreeze all of the layers.')
+                 #batch_size = 32 # note that more GPU memory is required after unfreezing the body
+                 print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
+                 local_model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+                     steps_per_epoch=max(1, len(lines_train)//batch_size),
+                     validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
+                     validation_steps=max(1, len(lines_val)//batch_size),
+                     epochs= end_epoch,
+                     initial_epoch= self.init_epoch,
+                     callbacks=[logging,checkpoint])
 
-                        #batch_size = 32 # note that more GPU memory is required after unfreezing the body
-                        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-                        local_model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
-                            steps_per_epoch=max(1, num_train//batch_size),
-                            validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
-                            validation_steps=max(1, num_val//batch_size),
-                            epochs= end_epoch,
-                            initial_epoch= init_epoch,
-                            callbacks=[logging,checkpoint])
+                 # clear session to free memory after each communication round
+                  
+             self.init_epoch += self.epoch
+             local_model.save_weights("".join([Client_dir,'local_model.h5']), overwrite=True)
+             '''client_data = pd.DataFrame({'name': [client_name], 'epoch': [int(server_data['epoch'])+epoch]})
+             client_data.to_csv("".join([Client_dir,'client_data.csv']), index=False)
+             #tf.saved_model.save(local_model, "".join([Client_dir,'local_model/']))
+             print('Client name: %s sent data for iteration %d to server......' %(client_name, init_epoch))'''
+             
+       
 
-                        # clear session to free memory after each communication round
-                         
-                    init_epoch += epoch
-                    local_model.save_weights("".join([Client_dir,'local_model.h5']), overwrite=True)
-                    '''client_data = pd.DataFrame({'name': [client_name], 'epoch': [int(server_data['epoch'])+epoch]})
-                    client_data.to_csv("".join([Client_dir,'client_data.csv']), index=False)
-                    #tf.saved_model.save(local_model, "".join([Client_dir,'local_model/']))
-                    print('Client name: %s sent data for iteration %d to server......' %(client_name, init_epoch))'''
-                    
-            
-
-
-class Data:
-
-    client_name = 'client_1'
-    
-    # parameters initializiation
-    def __init__(self, annotation_path= '/data/Annotation/list1.txt',val_split=0.1):
-      self.init_epoch, self.batch_size = 0, 32
-      self.annotation_path = annotation_path
-      self.val_split = val_split
-      
-    
-    # get the data from files
-    def read_data(annotation_path, val_split):
-        with open(annotation_path) as f:
-            lines = f.readlines()
-        np.random.seed(10101)
-        np.random.shuffle(lines)
-        np.random.seed(None)
-        num_val = int(len(lines)*val_split)
-        num_train = len(lines) - num_val
-        return lines[:,num_train], lines[num_train,:]
-
-    def data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes):
-        '''data generator for fit_generator'''
-        n = len(annotation_lines)
-        i = 0
-        while True:
-            image_data = []
-            box_data = []
-            for b in range(batch_size):
-                if i==0:
-                    np.random.shuffle(annotation_lines)
-                image, box = get_random_data(annotation_lines[i], input_shape, random=True)
-                image_data.append(image)
-                box_data.append(box)
-                i = (i+1) % n
-            image_data = np.array(image_data)
-            box_data = np.array(box_data)
-            y_true = preprocess_true_boxes(box_data, input_shape, anchors, num_classes)
-            yield [image_data, *y_true], np.zeros(batch_size)
-
-    def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, num_classes):
-        n = len(annotation_lines)
-        if n==0 or batch_size<=0: return None
-        return data_generator(annotation_lines, batch_size, input_shape, anchors, num_classes)
 
 
 if __name__ == "__main__":
    
     
-    smlp_local = YOLOV3()
+    #smlp_local = YOLOV3()
+    train = TrainingProcess(TrainDataReader(),Model())
     # number of local iteration before sending model to server
-    epoch = 1
     '''    
     x_client, y_client = read_files('/data/client_1X.pkl', '/data/client_1Y.pkl')
     
@@ -233,6 +239,6 @@ if __name__ == "__main__":
     # process and batch the test set  
     test_batched = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(len(y_test))
     '''                          
-    main(init_epoch, client_name, Server_dir, Client_dir, lines, iteration_num, (num_train,num_val,batch_size), epoch, anchors, num_classes, smlp_local)
+    #main(init_epoch, client_name, Server_dir, Client_dir, lines, iteration_num, (num_train,num_val,batch_size), epoch, anchors, num_classes, smlp_local)
     
     
