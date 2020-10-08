cd6b7c54269b880ca11e58e95a328392f5f6fb1f
Sheetalr24 <sheetal.reddy@ai.se>
2020-10-08 10:55:03 +0200

    Change to tf1.0

--- fl_interface/FEDn/start_data_storage.sh
+++ fl_interface/FEDn/start_data_storage.sh
@@ -4,4 +4,4 @@ COMPOSE_API_VERSION=1.40 \
 docker-compose --env-file project.env -f data-storage.yaml build --pull
 
 COMPOSE_API_VERSION=1.40 \
-docker-compose --env-file project.env -f data-storage.yaml up
+docker-compose --env-file project.env -f data-storage.yaml up --remove-orphans
