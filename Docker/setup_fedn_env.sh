394c26c3b33bfd761a08cb5c30f53ca0de0bf6e8
Sheetalr24 <sheetal.reddy@ai.se>
2020-10-12 11:45:56 +0200

    Add model_data to src

--- Docker/setup_fedn_env.sh
+++ Docker/setup_fedn_env.sh
@@ -3,4 +3,5 @@
 cp ../src/client1_new.py  ../fl_interface/FEDn/components/client/src/.
 cp  ../src/train.py  ../fl_interface/FEDn/components/client/src/.
 cp -r ../src/yolo3  ../fl_interface/FEDn/components/client/src/.
+cp -r ../src/yolo3/model_data  ../fl_interface/FEDn/components/client/src/.
 cp -r ../src/train.py  ../fl_interface/FEDn/components/client/src/.
