b5fe8e8206ca005b9c72f362d3c569445032b052
Sheetalr24 <sheetal.reddy@ai.se>
2020-10-12 09:59:19 +0200

    Setup integration env

---
+++ Docker/setup_fedn_env.sh
@@ -0,0 +1,6 @@
+#copy fedbird code into fedn 
+
+cp ../src/client1_new.py  ../fl_interface/FEDn/components/client/src/.
+cp  ../src/train.py  ../fl_interface/FEDn/components/client/src/.
+cp -r ../src/yolo3  ../fl_interface/FEDn/components/client/src/.
+cp -r ../src/train.py  ../fl_interface/FEDn/components/client/src/.
