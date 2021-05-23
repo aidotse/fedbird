c6f24579e095d9bf37aa400a797e82b333308ca8
Sheetalr24 <sheetal.reddy@ai.se>
2021-05-23 20:23:42 +0200

    delete previous code

--- Docker/setup_fedn_env.sh
+++
@@ -1,7 +0,0 @@
-#copy fedbird code into fedn 
-
-cp ../src/client1_new.py  ../fl_interface/FEDn/components/client/src/.
-cp  ../src/train.py  ../fl_interface/FEDn/components/client/src/.
-cp -r ../src/yolo3  ../fl_interface/FEDn/components/client/src/.
-cp -r ../src/yolo3/model_data  ../fl_interface/FEDn/components/client/src/.
-cp -r ../src/train.py  ../fl_interface/FEDn/components/client/src/.
