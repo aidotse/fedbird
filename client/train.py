b990163d709463c37affadbc237a78a8815d1767
Sheetalr24 <sheetal.reddy@ai.se>
2021-05-24 11:40:39 +0200

    Remove headers, comments, change train and val names

--- client/train.py
+++ client/train.py
@@ -1,15 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# File              : train.py
-# Author            : Sheetal Reddy <sheetal.reddy@ai.se>
-# Date              : 08.03.2021
-# Last Modified Date: 08.03.2021
-# Last Modified By  : Sheetal Reddy <sheetal.reddy@ai.se>
+
 import logging
 import sys
 from kerasmodel import create_seed_model
 import os
-#sys.path.append('/media/sheetal/project_space/FL/code/fedbird')
 
 if __name__ == '__main__':
     logger = logging.getLogger('__name__')
@@ -20,5 +15,5 @@ if __name__ == '__main__':
     weights = helper.load_model(sys.argv[1])
     model = create_seed_model('.')
     model.local_model.set_weights(weights)
-    model.train('/data/list1.txt','')
+    model.train('/data/train.txt','')
     helper.save_model(model.local_model.get_weights(), path=sys.argv[2])
