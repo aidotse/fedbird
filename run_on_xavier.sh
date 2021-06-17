c657abc06ff9de2f6f6457381166cb2e7aeacb0a
brahima <brahima.belem@qamcom.se>
2021-06-17 16:24:42 +0200

    Add pwd and change to xavier file

--- run_on_xavier.sh
+++ run_on_xavier.sh
@@ -1,2 +1,2 @@
-nvidia-docker run --gpus all --add-host=combiner:172.25.16.6  -v  /home/jetson/src1/new/fedn/test/fedbird/data/client1:/data fedbird-farallon3-client /bin/bash -c "fedn run client -in fedn-network.yaml"
+nvidia-docker run --gpus all --add-host=combiner:172.25.16.6  -v  $PWD/data/client1:/data fedbird-farallon3-client /bin/bash -c "fedn run client -in fedn-network-xavier.yaml"
 
