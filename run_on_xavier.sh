35cea428ef6b503af74316bb2b9277584925b289
Sheetalr24 <sheetal.reddy@ai.se>
2021-06-21 16:55:00 +0200

    Add documentation for client2

--- run_on_xavier.sh
+++ run_on_xavier.sh
@@ -1,2 +1,7 @@
+#  To run on client2 
+# 1. Change the client1 to client2
+# 2. Change farallon3 to roaster3
+
+
 nvidia-docker run --gpus all --add-host=combiner:172.25.16.6  -v  $PWD/data/client1:/data fedbird-farallon3-client /bin/bash -c "fedn run client -in fedn-network-xavier.yaml"
 
