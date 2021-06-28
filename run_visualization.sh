500f34f301628029859f4afaff5edcdf4427fa93
Sheetalr24 <sheetal.reddy@ai.se>
2021-06-28 09:40:32 +0200

    Upload video for visualization

--- run_visualization.sh
+++ run_visualization.sh
@@ -1,6 +1,6 @@
 #converts npz to h5 model
-docker run  -v  $PWD/client:/client -v $PWD/data:/data  -it  fedbird-farallon3-client python3 /client/npz_to_h5.py /data/global_model /data/global_model.h5 
+docker run  -v  $PWD/client:/client -v $PWD/model:/model  -it  fedbird-farallon3-client python3 /client/npz_to_h5.py /model/global_model /model/global_model.h5 
 
 #uses the .h5 model and infers it on the video
-nvidia-docker run  -v  $PWD/client:/client -v $PWD/data:/data  -it  fedbird-farallon3-client python3 /client/visualize.py --model /data/global_model.h5 --input /data/video.mp4 --output /data/output.mp4
+nvidia-docker run  -v  $PWD/client:/client -v $PWD/video:/video  -v $PWD/model:/model -it  fedbird-farallon3-client python3 /client/visualize.py --model /model/global_model.h5 --input /video/video.mp4 --output /video/output.mp4
 
